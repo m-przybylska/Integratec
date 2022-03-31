e9ee73cf147eeb070e9a6a7c347f6962949918c0
Aleknat <aleknat266gmail.com>
2022-03-31 16:34:00 +0200

    Frontend to backend connection

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,6 +2,31 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import axios from "axios"
+
+const AccountProfiles = () => {
+	const [accountProfiles, setAccountProfiles] = useState([]);
+
+	const fetchAccountProfiles = () => {
+		axios.get("http://localhost:8080/").then(res => {
+			setAccountProfiles(res.data);
+		});
+	}
+	
+	useEffect(() => {
+		fetchAccountProfiles();
+	}, []);
+
+	return accountProfiles.map((accountProfile, index) => {
+		return (
+			<div key={index}>
+				<h1>{accountProfile.login}</h1>
+				<h1>{accountProfile.password}</h1>
+			</div>
+		);
+	});
+}
 
 function App() {
 	return (
@@ -14,6 +39,7 @@ function App() {
 			</Router>
 
 			<h1 className="App-header">Hello World!</h1>
+			<h1 className="App-header"><AccountProfiles /></h1>
 		</div>
 	);
 }
