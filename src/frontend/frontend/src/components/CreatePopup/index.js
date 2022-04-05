a6a3148f8ab7af44b978be905f480af28357f743
patekphilippe <35869206+patekphilippe@users.noreply.github.com>
2022-04-05 19:17:50 +0200

    PopupCreator, Navbar

---
+++ src/frontend/frontend/src/components/CreatePopup/index.js
@@ -0,0 +1,3 @@
+import CreatePopup from './CreatePopup';
+
+export default CreatePopup;
---
+++ src/frontend/frontend/src/components/PopupBackGround/PopupBackGround.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from "react";
+import CreatePopup from "../CreatePopup";
+import "./PopupBackGround.scss";
+
+class PopupBackGround extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div
+        className={
+          this.props.popupIsVisible
+            ? "PopupBackGround"
+            : "PopupBackGround hidden"
+        }
+        onClick={() => this.props.setPopupVisibility()}
+      ></div>
+    );
+  }
+}
+
+export default PopupBackGround;
