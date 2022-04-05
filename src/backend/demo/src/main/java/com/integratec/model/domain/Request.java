86295cbe5f0f5ffaea3f3939111e7a351dea1822
m-przybylska <magda.przybylska@poczta.onet.pl>
2022-04-06 00:15:52 +0200

    javax and hibernate validators- dependencies and annotations

--- src/backend/demo/src/main/java/com/integratec/model/domain/Request.java
+++ src/backend/demo/src/main/java/com/integratec/model/domain/Request.java
@@ -3,7 +3,9 @@ package com.integratec.model.domain;
 import lombok.*;
 
 import java.util.Date;
-
+import javax.validation.constraints.*;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
 
 @Entity
@@ -15,19 +17,30 @@ import javax.persistence.*;
 @ToString
 public class Request {
     @Id
+    @NotNull
+    @Range(min = 1, max = 999999)
     @SequenceGenerator(name = "requestSequence", sequenceName = "requestSequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "requestSequence")
     private Long requestId;
+    @NotNull
+    @Range(min = 1, max = 999)
     @Column(name = "Receiver")
     private Long receiver;
+    @NotNull
+    @Range(min = 1, max = 999)
     @Column(name = "Sender")
     private Long sender;
+    @NotEmpty
+    @Size(min = 3, max = 70)
     @Column(name = "Title")
     private String title;
+    @Size(max = 1000)
     @Column(name = "Text")
     private String text;
+    @Size(max = 500)
     @Column(name = "Comment")
     private String comment;
+    @DateTimeFormat(pattern="dd/MM/yyyy")
     @Column(name = "SendDate")
     @Temporal(TemporalType.DATE)
     private Date sendDate;
--- src/backend/demo/src/main/java/com/integratec/services/RequestService.java
+++ src/backend/demo/src/main/java/com/integratec/services/RequestService.java
@@ -1,6 +1,7 @@
 package com.integratec.services;
 
 import java.util.List;
+import java.util.Set;
 
 
 import com.integratec.model.domain.Request;
@@ -8,6 +9,9 @@ import com.integratec.model.repositories.RequestRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import javax.validation.*;
 
 @Service
 public class RequestService {
@@ -15,16 +19,33 @@ public class RequestService {
     private final RequestRepository requestRepository;
 
     @Autowired
-    public RequestService(RequestRepository requestRepository)
-    {
-        this.requestRepository= requestRepository;
+    public RequestService(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
     }
-    
-    public List<Request> getRequests(){
-		return requestRepository.findAll();
-	}
 
-    public Request postRequest( Request newRequest){
+    public List<Request> getRequests() {
+        return requestRepository.findAll();
+    }
+
+    private void isValid(Request request){
+        Validator validator = createValidator();
+        Set<ConstraintViolation<Request>> violations = validator.validate(request);
+        if (violations.size() == 0) {
+            return;
+        } else {
+
+        }
+    }
+    public static Validator createValidator() {
+        Configuration<?> config = Validation.byDefaultProvider().configure();
+        ValidatorFactory factory = config.buildValidatorFactory();
+        Validator validator = factory.getValidator();
+        factory.close();
+        return validator;
+    }
+
+    public Request postRequest(@Valid Request newRequest) {
+        isValid(newRequest);
         return requestRepository.save(newRequest);
     }
 }
