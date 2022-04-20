2d7d4c50279b9a9a1242354d9e8bb60fee0734a8
m-przybylska <magda.przybylska@poczta.onet.pl>
2022-04-20 14:33:32 +0200

    rest controller advice

--- src/backend/demo/src/main/java/com/integratec/controllers/ExceptionControllerAdvice.java
+++ src/backend/demo/src/main/java/com/integratec/controllers/ExceptionControllerAdvice.java
@@ -1,9 +1,22 @@
 package com.integratec.controllers;
+import motif.errormessage.ErrorMessage;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import static com.sun.beans.introspect.PropertyInfo.Name.description;
 
 @RestControllerAdvice
 class ExceptionControllerAdvice {
 
+    @ExceptionHandler(value = {ResourceNotFoundException.class, Exception.class})
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(ex.getMessage(), request.getDescription(true));
 
-
+        return message;
+    }
 }
