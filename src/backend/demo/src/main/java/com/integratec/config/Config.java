5e7355acd4da6393c2ab5e9230b951e8bb5186b2
m-przybylska <magda.przybylska@poczta.onet.pl>
2022-04-26 11:49:41 +0200

    class AccountConfig removed

---
+++ src/backend/demo/src/main/java/com/integratec/config/Config.java
@@ -0,0 +1,22 @@
+package com.integratec.config;
+
+import com.integratec.controllers.AccountController;
+import com.integratec.controllers.RequestController;
+import com.integratec.model.repositories.AccountRepository;
+import com.integratec.model.repositories.RequestRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Config {
+    @Bean
+    CommandLineRunner commandLineRunner(AccountRepository accountRepository, RequestRepository requestRepository,
+                                        AccountController controller, RequestController requestController) throws Exception {
+        return args -> {
+        };
+
+    }
+}
--- src/backend/demo/src/main/java/com/integratec/controllers/ExceptionControllerAdvice.java
+++ src/backend/demo/src/main/java/com/integratec/controllers/ExceptionControllerAdvice.java
@@ -1,20 +1,38 @@
 package com.integratec.controllers;
+
 import motif.errormessage.ErrorMessage;
-import org.apache.velocity.exception.ResourceNotFoundException;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 class ExceptionControllerAdvice {
 
-    @ExceptionHandler(value = {ResourceNotFoundException.class, Exception.class})
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class, Exception.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Map<String, String> methodArgumentNotValidException(@NotNull MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+        /*@ExceptionHandler(value = {MethodArgumentNotValidException.class, Exception.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessage methodArgumentNotValidException(@NotNull MethodArgumentNotValidException ex, @NotNull WebRequest request) {
         ErrorMessage message = new ErrorMessage(ex.getMessage(), request.getDescription(true));
         return message;
-    }
+    }*/
+
 }
