d17aad19936fce1e505e1ba9a93e6da7b8c707d4
m-przybylska <magda.przybylska@poczta.onet.pl>
2022-04-26 22:01:48 +0200

    pom.xml cleaned

--- src/backend/demo/src/main/java/com/integratec/controllers/ExceptionControllerAdvice.java
+++ src/backend/demo/src/main/java/com/integratec/controllers/ExceptionControllerAdvice.java
@@ -1,6 +1,4 @@
 package com.integratec.controllers;
-
-import motif.errormessage.ErrorMessage;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -8,8 +6,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-
 import java.util.HashMap;
 import java.util.Map;
 
