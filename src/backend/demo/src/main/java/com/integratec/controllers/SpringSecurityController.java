387da53317f08277197c1cc9d92088d7e0464fce
Lukasz Lisowski <l.lisowski20@gmail.com>
2022-04-28 19:04:04 +0200

    Spring security login using database

--- src/backend/demo/src/main/java/com/integratec/controllers/SpringSecurityController.java
+++ src/backend/demo/src/main/java/com/integratec/controllers/SpringSecurityController.java
@@ -2,13 +2,10 @@ package com.integratec.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.LockedException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.integratec.security.AccountDetailsService;
 
 @Controller
 public class SpringSecurityController {
