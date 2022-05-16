819596f9814db25999988b13e4618c5f10449921
Aleknat <aleknat266gmail.com>
2022-05-16 22:11:18 +0200

    adding CORS

--- src/backend/demo/src/main/java/com/integratec/config/Config.java
+++ src/backend/demo/src/main/java/com/integratec/config/Config.java
@@ -3,15 +3,18 @@ package com.integratec.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
 @EnableSwagger2
-public class Config {
-    @Bean
-    CommandLineRunner commandLineRunner() throws Exception {
-        return args -> {
-        };
+@Configuration
+@EnableWebMvc
+public class Config implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 }
