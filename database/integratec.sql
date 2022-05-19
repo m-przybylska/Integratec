22731622a43e99a07c7d8b70fd68296ef67ade23
m-przybylska <magda.przybylska@poczta.onet.pl>
2022-05-19 15:13:49 +0200

    camelcase to snake

--- database/integratec.sql
+++ database/integratec.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS Request;
-DROP TABLE IF EXISTS Account;
-DROP TABLE IF EXISTS RequestCategory;
-DROP TABLE IF EXISTS RequestStatus;
-DROP TABLE IF EXISTS RequestPriority;
-DROP TRIGGER IF EXISTS RequestCategoryTrigger;
+DROP TABLE IF EXISTS request;
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS request_category;
+DROP TABLE IF EXISTS request_status;
+DROP TABLE IF EXISTS request_priority;
+DROP TRIGGER IF EXISTS request_category_trigger;
 
-CREATE TABLE `Account` (
+CREATE TABLE `account` (
 `user_account_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `login` varchar(30) NOT NULL,
 `password` varchar(60) NOT NULL,
@@ -13,22 +13,22 @@ CREATE TABLE `Account` (
 `surname` varchar(30) NOT NULL
 );
 
-CREATE TABLE `RequestCategory` (
+CREATE TABLE `request_category` (
 `request_category_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `request_category` varchar(30) NOT NULL
 );
 
-CREATE TABLE `RequestStatus` (
+CREATE TABLE `request_status` (
 `request_status_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `request_status` varchar(30) NOT NULL
 );
 
-CREATE TABLE `RequestPriority` (
+CREATE TABLE `request_priority` (
 `request_priority_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `request_priority` varchar(30) NOT NULL
 );
 
-CREATE TABLE `Request` (
+CREATE TABLE `request` (
 `request_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `title` varchar(70) NOT NULL,
 `sender_id` int NOT NULL,
@@ -42,32 +42,32 @@ CREATE TABLE `Request` (
 KEY `k_request_status` (`request_status_id`),
 KEY `k_request_category` (`request_category_id`),
 KEY `k_request_priority` (`request_priority_id`),
-CONSTRAINT `k_request_status` FOREIGN KEY (`request_status_id`) REFERENCES `RequestStatus` (`request_status_id`),
-CONSTRAINT `k_request_category` FOREIGN KEY (`request_category_id`) REFERENCES `RequestCategory` (`request_category_id`),
-CONSTRAINT `k_request_priority` FOREIGN KEY (`request_priority_id`) REFERENCES `RequestPriority` (`request_priority_id`)
+CONSTRAINT `k_request_status` FOREIGN KEY (`request_status_id`) REFERENCES `request_status` (`request_status_id`),
+CONSTRAINT `k_request_category` FOREIGN KEY (`request_category_id`) REFERENCES `request_category` (`request_category_id`),
+CONSTRAINT `k_request_priority` FOREIGN KEY (`request_priority_id`) REFERENCES `request_priority` (`request_priority_id`)
 );
 
-DELIMITER // 
-CREATE TRIGGER RequestCategoryTrigger
+DELIMITER //
+CREATE TRIGGER request_category_trigger
 BEFORE INSERT ON request
 FOR EACH ROW
 BEGIN
-IF NEW.request_category_id = 6 OR NEW.request_category_id = 7 
-OR NEW.request_category_id = 8 OR NEW.request_category_id = 14 
+IF NEW.request_category_id = 6 OR NEW.request_category_id = 7
+OR NEW.request_category_id = 8 OR NEW.request_category_id = 14
 OR NEW.request_category_id = 15 THEN SET NEW.receiver_id = 2;
-ELSE 
+ELSE
 SET NEW.receiver_id = 1;
-END IF; 
+END IF;
 END//
 DELIMITER ;
 
-insert into Account(login, password, name, surname)
+insert into account(login, password, name, surname)
 values
 ("Stanley_Kubirick", "$2a$10$rLQX0KuS9fdOQ3jfxuzN5uBPaWv4HHLptrrf2EQDD9uexBu7SUn3e", "Stan", "Kub"),
 ("Quentin_Tarantino", "$2a$10$rLQX0KuS9fdOQ3jfxuzN5uBPaWv4HHLptrrf2EQDD9uexBu7SUn3e", "Que", "Tar"),
 ("Sergio_Leone", "$2a$10$rLQX0KuS9fdOQ3jfxuzN5uBPaWv4HHLptrrf2EQDD9uexBu7SUn3e", "Ser", "Leo");
 
-insert into RequestCategory(request_category)
+insert into request_category(request_category)
 values
 ("kitchen"),
 ("stationery"),
@@ -85,21 +85,21 @@ values
 ("business travels"),
 ("breakdowns & technical matter");
 
-insert into RequestPriority(request_priority)
+insert into request_priority(request_priority)
 values
 ("unspecified"),
 ("today"),
 ("this week"),
 ("this month");
 
-insert into RequestStatus(request_status)
+insert into request_status(request_status)
 values
 ("new"),
 ("done"),
 ("in progress"),
 ("to do");
 
-insert into Request(title, sender_id, receiver_id, text, comment, send_date, request_category_id, request_priority_id, request_status_id) 
+insert into request(title, sender_id, receiver_id, text, comment, send_date, request_category_id, request_priority_id, request_status_id)
 values ("bulaala", "1", "2", "skonczyly sie ciasteczka:(", "komentarz 1", "2022-04-22", "6", "1", "1"),
 ("title1", "1", "1", "skonczyly sie ciasteczka:(", "komentarz 1", "2022-04-22", "7", "1", "2"),
 ("title2", "1", "3", "skonczyly sie ciasteczka:(", "komentarz 1", "2022-04-22", "8", "1", "3"),
